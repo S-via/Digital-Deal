34cd3bf8f0dc8afc8a2127e7c4265b895804bd30
Gustavo Martinez <gustavo.mrtnz99@gmail.com>
2024-12-05 19:24:04 -0800

    added code to typedefs. Added the type Event, User and Auth with its properties passed in. Also Input for and evene and comment and the mutation for created event, login, signup, ctreate comment and add friend

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,61 @@
+const typeDefs = `
+ type User {
+  _id: ID!
+  username: String!
+  email: String!
+  password: String!
+  hostedEvents: [Event]
+  joinedEvents: [Event]
+  friends; [User]
+ }
+
+ type Event {
+  _id: ID!
+  firstTeam: String!
+  secondTeam: String!
+  description: Text
+  odds: Float!
+  comments: [Comment]
+  competitors: [User]
+ }
+
+ type Comments {
+  _id: ID!
+  user: User!
+  text: String!
+ }
+
+ type Auth{
+  token: ID!
+  user: User!
+ }
+
+ type Query {
+  user(_id: ID!): Profile
+  event(user: ID!): [Event]
+  comments(user: ID!): Comment
+ }
+
+ input EventInput {
+  firstTeam: String!
+  secondTeam: String!
+  description: String!
+  odds: Int!
+ }
+ 
+ input CreatedComment {
+  event: ID!
+  text: String!
+ }
+
+ type Mutation {
+  createEvent(eventInput: EventInput!): User
+  login(email: String, password: String): Auth
+  signup(username: String, email: String, password: String): User
+  createComment(createdComment: CreatedComment): User
+  addFriend(username: String!): [User]
+  deleteEvent(_id: ID!): Event
+ }
+`
+
+module.exports = typeDefs
