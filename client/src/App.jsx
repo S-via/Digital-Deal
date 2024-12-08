ee6553c936f806d81a7822e1003ddc25c5021e1e
Ohkrah <dion.cobb.thornhill@gmail.com>
2024-12-08 14:40:38 -0800

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,17 +7,32 @@ import {
   createHttpLink,
 } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
-
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 import Header from './components/header';
+const authLink = setContext((_, { headers }) => {
+ const token = localStorage.getItem('id_token');
+ return {
+   headers: {
+     ...headers,
+     authorization: token ? `Bearer ${token}` : '',
+   },
+ };
+});
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
 
   return (
-    <>
+    <ApolloProvider client={client}>
     <Header/>
       <Outlet />
-    </>
+    </ApolloProvider>
   )
 }
 
