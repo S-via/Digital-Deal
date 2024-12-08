b4419d37fd44ff395a5168602f0accba9ffbe2f5
S-via <168160925+S-via@users.noreply.github.com>
2024-12-08 13:01:51 -0800

    loginSignup

--- client/src/components/LoginSign.jsx
+++ client/src/components/LoginSign.jsx
@@ -1,22 +1,30 @@
-import { } from '@chakra-ui/react';
+import {Box,Flex,FormControl,FormLabel,Input,Button,Heading } from '@chakra-ui/react';
 
 import { useState } from 'react';
-function loginSignupCard () {
+function loginSignupPage() {
 
 const [isFlipped, setFlipped] = useState(false);
-const handleFlip =() => setIsFlipped(!isFlipped);
+const handleFlip = () => setFlipped(!isFlipped);
 
 return (
-    <Box className={}>
-        <Flex className={`relative w-[350px] h-[500px] transition-transform duration-700 transform ${
-          isFlipped ? 'rotate-y-180' : ''
-        }`}
-        justify="center">
+    <Box display="flex" justifyContent="center" alignItems="center">
+        <Flex className={`relative`}
+         width="350px"
+         height="500px" 
+         transform ={`perspective(1000px)} ${
+          isFlipped ? 'rotateY(180deg)' : ''}`}
+        position="relative"
+        transition="transform 0.7s">
             
             {/*SignUp */}
-            <Box className={`absolute w-full h-full bg-white text-black p-6 rounded-lg transition-all duration-700 transform ${
-            isFlipped ? 'rotate-y-180' : ''
-          }` }>
+            <Box
+            width="100px"
+            height="100px"
+            transform ={`rotateY (${
+                isFlipped ? 180:0} deg)`}
+                backfaceVisibility="hidden"
+                transition="transform 0.75">
+
                 <Heading>SignUp</Heading>
                 <FormControl><FormLabel>Username</FormLabel>
                     <Input />
@@ -32,9 +40,14 @@ return (
             </Box>
         </Flex>
 
-        <Box className={`absolute w-full h-full bg-white text-black p-6 rounded-lg transition-all duration-700 transform rotate-y-180 ${
-            isFlipped ? 'rotate-y-0' : 'rotate-y-180'
-          }` }>
+        <Box
+        width="100px"
+        height="100px"
+        transform ={`rotateY (${
+            isFlipped ? 0: -180} deg)`}
+            backfaceVisibility="hidden"
+            transition="transform 0.75">
+        
             <Heading>Log In</Heading>
             <FormControl><FormLabel>Email</FormLabel>
                 <Input />
@@ -42,10 +55,11 @@ return (
             <FormControl><FormLabel>Password</FormLabel>
                 <Input />
             </FormControl>
+            <Button colorScheme="blue" onClick={handleFlip}>Sign Up</Button>
 
         </Box>
     </Box>
 )
 
 }
-export default loginSignupCard;
+export default loginSignupPage;
